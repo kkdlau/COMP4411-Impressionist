e1c9b6f7130e54671932292406dce62816bce3e5
Danny Lau <danny1251597530@gmail.com>
2022-02-24 21:27:07 +0800

    video reading works

---
+++ VideoUtils.cpp
@@ -0,0 +1,53 @@
+#include "VideoUtils.hpp"
+
+using namespace VideoUtils;
+
+PAVIFILE VideoUtils::pfile;
+PAVISTREAM VideoUtils::stream;
+AVIFILEINFOA VideoUtils::info;
+long VideoUtils::current_frame = 0;
+long VideoUtils::max_frames = 0;
+
+void VideoUtils::get_video_info() {
+	AVIFileInfo(pfile, &info, sizeof(AVIFILEINFO));
+}
+
+long VideoUtils::num_frames() {
+    return AVIStreamLength(stream);
+}
+
+void VideoUtils::open_video(const char* video_path) {
+    AVIFileInit();
+    current_frame = 0;
+    max_frames = 0;
+    AVIFileOpen(&pfile, video_path, OF_READ, NULL);
+    HRESULT hr = AVIStreamOpenFromFile(&stream, video_path, streamtypeVIDEO, 0, OF_READ, NULL);
+    get_video_info();
+    max_frames = num_frames();
+}
+
+auto VideoUtils::get_frame(int frame_index) {
+    current_frame = frame_index;
+    BITMAPINFOHEADER bih;
+    ZeroMemory(&bih, sizeof(BITMAPINFOHEADER));
+    bih.biBitCount = 24;
+    bih.biClrImportant = 0;
+    bih.biClrUsed = 0;
+    bih.biCompression = BI_RGB;
+    bih.biPlanes = 1;
+    bih.biSize = 40;
+    bih.biXPelsPerMeter = 0;
+    bih.biYPelsPerMeter = 0;
+    bih.biSizeImage = (((bih.biWidth * 3) + 3) & 0xFFFC) * bih.biHeight;
+
+    PGETFRAME pframe = AVIStreamGetFrameOpen(stream, NULL);
+    BYTE* pDIB = (BYTE*)AVIStreamGetFrame(pframe, current_frame);
+    RtlMoveMemory(&bih.biSize, pDIB, sizeof(BITMAPINFOHEADER));
+    BYTE* Bits = new BYTE[bih.biSizeImage];
+    RtlMoveMemory(Bits, pDIB + sizeof(BITMAPINFOHEADER), bih.biSizeImage);
+
+    return Bits;
+    //Image tmp;
+    //tmp.set(Bits, bih.biWidth, bih.biHeight);
+    //return tmp;
+}
--- VideoUtils.hpp
+++ VideoUtils.hpp
@@ -1,32 +1,24 @@
-#pragma once
-#include <Vfw.h>
+#ifndef __VIDEO__
 
+#define __VIDEO__
+
+#include <Vfw.h>
+#pragma comment(lib, "Vfw32.lib")
 
 namespace VideoUtils {
-	static void open_video(const char* video_path, PAVISTREAM& stream, PAVIFILE& pfile) {
-		AVIFileInit();
-		AVIFileOpen(&pfile, video_path, OF_READ, NULL);
-		HRESULT hr = AVIStreamOpenFromFile(&stream, video_path, streamtypeVIDEO, 0, OF_READ, NULL);
-	}
 
-	static void get_video_info(PAVIFILE& pfile, AVIFILEINFO& info) {
-		AVIFileInfo(pfile, &info, sizeof(AVIFILEINFO));
-	}
+	extern PAVIFILE pfile;
+	extern PAVISTREAM stream;
+	extern AVIFILEINFOA info;
+	extern long current_frame;
+	extern long max_frames;
 
-	static auto get_frame_pointer(PAVIFILE& pfile, PAVISTREAM& stream, AVIFILEINFO& info) {
-		BITMAPINFOHEADER bi;
-		long format_length = sizeof(bi);
-		AVIStreamReadFormat(stream, 0, &bi, &format_length);
-		bi.biBitCount = 24;
-		bi.biCompression = BI_RGB;
-		bi.biSizeImage = 0;
-		PGETFRAME get_frame = AVIStreamGetFrameOpen(stream, &bi);
-		void* buffer = AVIStreamGetFrame(get_frame, 0);
-		const long num_bytes = info.dwWidth * info.dwHeight * 3;
-		unsigned char* bitmap = new unsigned char[num_bytes];
+	void get_video_info();
 
-		CopyMemory(bitmap, (char*)buffer + ((BITMAPINFOHEADER*)buffer)->biSize, num_bytes);
+	long num_frames();
 
-		return bitmap;
-	}
+	void open_video(const char* video_path);
+
+	auto get_frame(int frame_index);
 };
+#endif // !__VIDEO__
